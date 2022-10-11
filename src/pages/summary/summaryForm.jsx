3c68475168abf84e0a738c1691eae0242e27b308
gamzeipek <ipek@teknopar.com.tr>
2022-10-11 10:15:37 +0300

    Mock worker setup

--- src/pages/summary/summaryForm.jsx
+++ src/pages/summary/summaryForm.jsx
@@ -1,13 +1,31 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
+
+// const Example = () => (
+//   <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+//     <Button variant="success">Click me to see</Button>
+//   </OverlayTrigger>
+// );
 
 const SummaryForm = () => {
   const [disabled, setDisabled] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Popover right</Popover.Header>
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const chLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+      I agree to{" "}
+      <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
   return (
